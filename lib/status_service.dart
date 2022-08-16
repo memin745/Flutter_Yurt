3841cfe20bf44d322e7b9abb24c521a072379641
Omer Faruk <omerfarukiskk@gamil.com>
2022-08-16 16:33:11 +0300

    status_service.dart

---
+++ lib/status_service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StatusService{
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+
+
+  Stream<QuerySnapshot>getStatus(){
+    var ref = _firestore.collection("users").snapshots();
+    return ref;
+  }
+}
