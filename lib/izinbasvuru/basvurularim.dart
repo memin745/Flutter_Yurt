71275b5e1c080daad498b51624e65bd919858cfc
Omer Faruk <omerfarukiskk@gamil.com>
2022-08-25 13:01:06 +0300

    basvurularim

--- lib/izinbasvuru/basvurularim.dart
+++ lib/izinbasvuru/basvurularim.dart
@@ -5,20 +5,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_3/homepage.dart';
 import 'package:flutter_application_3/izinbasvuru/basvurular.dart';
 
-
 class BasvurularimPage extends StatefulWidget {
-  final  String takenvalue;
+  final String takenvalue;
   final String postValue;
   final String postValue2;
-  const BasvurularimPage({Key key,  this.takenvalue,this.postValue,this.postValue2}) : super(key: key);
-   
+  const BasvurularimPage(
+      {Key key, this.takenvalue, this.postValue, this.postValue2})
+      : super(key: key);
 
   @override
   State<BasvurularimPage> createState() => _BasvurularimPageState();
 }
 
 class _BasvurularimPageState extends State<BasvurularimPage> {
-  final _firestore =FirebaseFirestore.instance;
+  final _firestore = FirebaseFirestore.instance;
   final FirebaseAuth auth = FirebaseAuth.instance;
   String name = "Name Loading...";
   String email = "Email Loading...";
@@ -59,7 +59,7 @@ class _BasvurularimPageState extends State<BasvurularimPage> {
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference basvuruRef =_firestore.collection("AlinanBasvurular");
+    CollectionReference basvuruRef = _firestore.collection("AlinanBasvurular");
     Size size = MediaQuery.of(context).size;
     int _currentIndex = 0;
     return Scaffold(
@@ -67,11 +67,12 @@ class _BasvurularimPageState extends State<BasvurularimPage> {
         backgroundColor: Color(0xFF808080),
         title: Text("Şehit Furkan Doğan Yurdu"),
         automaticallyImplyLeading: false,
-        leading: new IconButton(onPressed: () => Navigator.pushReplacement(
+        leading: new IconButton(
+          onPressed: () => Navigator.pushReplacement(
             //Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => BavurularPage()),
-        ),
+          ),
           icon: new Icon(Icons.arrow_back, color: Colors.white),
         ),
       ),
@@ -81,60 +82,66 @@ class _BasvurularimPageState extends State<BasvurularimPage> {
             image: AssetImage("assets/i4.jpeg"),
             fit: BoxFit.cover,
           ),
-      ),
+        ),
         width: size.width * 1,
         height: size.height * 1,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [Container(
-            margin: EdgeInsets.only(top:size.height *0.02),
-            decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(15),image: DecorationImage(
-          image: AssetImage(
-              'assets/test.png'),
-          fit: BoxFit.fill,),
-            ),
-            width: size.width * 0.60,
-            height: size.height *0.30,
-          ),
-          Container(
-                margin: EdgeInsets.only(top: 15),
-                decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(15)),
-                width: size.width * 0.80,
-                height: size.height * 0.30,
-                child: ListView(
-                  scrollDirection: Axis.vertical,
-                  children: [
-                    Container(child: Text(widget.postValue,textAlign: TextAlign.center, style:TextStyle(fontSize: 20,fontWeight: FontWeight.w800,color: Colors.white),),)
-                  ]
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: size.height * 0.02),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                  image: AssetImage('assets/test.png'),
+                  fit: BoxFit.fill,
                 ),
-              
-              
               ),
-          Container(
-            width: size.width * 0.35,
-            height: size.height*0.06,
-            margin:EdgeInsets.only(top: size.height * 0.05,),
-            decoration: BoxDecoration(borderRadius : BorderRadius.circular(20),color: Colors.white),
-            child:  TextButton(onPressed:() async{
-
-             
-
-                Map<String, String> movieData = {
-           
-                  
-              };
-              FirebaseAuth.instance;
-              await basvuruRef.doc(widget.postValue2).set({"Adı":name,"Email":email,"Telefon":soyadi});
-              },
-              
-              
+              width: size.width * 0.60,
+              height: size.height * 0.30,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(15)),
+              width: size.width * 0.80,
+              height: size.height * 0.30,
+              child: ListView(scrollDirection: Axis.vertical, children: [
+                Container(
                   child: Text(
-                    "Başvur",
-                    style: TextStyle(fontSize: 20, color: Colors.black),
+                    widget.postValue,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white),
                   ),
+                )
+              ]),
+            ),
+            Container(
+              width: size.width * 0.35,
+              height: size.height * 0.06,
+              margin: EdgeInsets.only(
+                top: size.height * 0.05,
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), color: Colors.white),
+              child: TextButton(
+                onPressed: () async {
+                  Map<String, String> movieData = {};
+                  FirebaseAuth.instance;
+                  await basvuruRef
+                      .doc(widget.postValue2)
+                      .set({email:FieldValue.arrayUnion([name,email,soyadi])});
+                },
+                child: Text(
+                  "Başvur",
+                  style: TextStyle(fontSize: 20, color: Colors.black),
                 ),
-          ),
-          
+              ),
+            ),
           ],
         ),
       ),
