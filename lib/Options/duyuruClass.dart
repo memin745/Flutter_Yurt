2a1086c9dce94b5f3455471382ca6c3fa38c8812
Meminyagmur <muhammeteminyagmur@gmail.com>
2022-09-07 10:53:25 +0300

    duyuru durumu

--- lib/Options/duyuruClass.dart
+++ lib/Options/duyuruClass.dart
@@ -31,58 +31,66 @@ class duyuru extends StatelessWidget {
                     String postValue = "${mypost['Metin']}";
 
                     Future<void> _showChoiseDialog(BuildContext context) {}
-
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: InkWell(
-                        onTap: () {
-                          _showChoiseDialog(context);
-                        },
-                        child: Container(
-                          height: size.height * .2,
-                          decoration: BoxDecoration(
-                              color: Color(0xFFeeeee0),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.50),
-                                  blurRadius: 20,
-                                  offset: Offset(0, 4),
-                                )
-                              ],
-                              border: Border.all(color: Colors.blue, width: 2),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20))),
-                          child: ListView(
-                            scrollDirection: Axis.vertical,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Container(
-                                  child: TextButton(
-                                    onPressed: () => Navigator.pushReplacement(
-                                      //Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => DuyuruMetniPage(
-                                                postValue: postValue,
-                                              )),
-                                    ),
-                                    child: Text(
-                                      postValue,
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w800,
-                                          color: Colors.black),
+                    if (mypost['Duyuru Durumu'] == false) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          onTap: () {
+                            _showChoiseDialog(context);
+                          },
+                          child: Container(
+                            height: size.height * .2,
+                            decoration: BoxDecoration(
+                                color: Color(0xFFeeeee0),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.50),
+                                    blurRadius: 20,
+                                    offset: Offset(0, 4),
+                                  )
+                                ],
+                                border:
+                                    Border.all(color: Colors.blue, width: 2),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            child: ListView(
+                              scrollDirection: Axis.vertical,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Container(
+                                    child: TextButton(
+                                      onPressed: () =>
+                                          Navigator.pushReplacement(
+                                        //Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                DuyuruMetniPage(
+                                                  postValue: postValue,
+                                                )),
+                                      ),
+                                      child: Text(
+                                        postValue,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w800,
+                                            color: Colors.black),
+                                      ),
                                     ),
                                   ),
-                                ),
-                              )
-                            ],
+                                )
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
+                      );
+                    }
+                    else{
+                      mypost['Duyuru Durumu'] == false;
+                      return Text("",style: TextStyle(fontSize: 1),);
+                    }
                   });
         },
       ),
